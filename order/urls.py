3b958004ac08acb82008ac97bd3702ffb800afad
Lorenzo <lorenzoplaypereira@gmail.com>
2025-07-16 23:26:48 -0300

    fix exercise

--- order/urls.py
+++ order/urls.py
@@ -5,6 +5,6 @@ from rest_framework import routers
 from order import viewsets
 
 router = routers.SimpleRouter()
-router.register(r"order", viewsets.OrderViewSet, basename="order")
+router.register(r"order", viewsets.OrderViewSet, basename="orders")
 
 urlpatterns = [path("", include(router.urls))]
